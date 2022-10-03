6ca4535dbd11fba0900e8d444742bfdfce94445f
hansol <102638663+katf101@users.noreply.github.com>
2022-10-04 00:08:30 +0900

    app.tsx css 적용

--- Simple-Todo-App/src/App.tsx
+++ Simple-Todo-App/src/App.tsx
@@ -3,11 +3,12 @@ import TodoStore from "./stores/TodoStore";
 import TodoInput from "./Todo/TodoInput";
 import TodoList from "./Todo/TodoList";
 
+// TodoStore를 가져와 todos에 할당
 const todos = new TodoStore();
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <TodoInput todos={todos} />
       <TodoList todos={todos} />
     </div>
