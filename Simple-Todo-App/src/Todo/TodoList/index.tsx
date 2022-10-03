1eb1f7a650512dd2b84306001e3136f5a1b8c3d5
hansol <102638663+katf101@users.noreply.github.com>
2022-10-04 00:09:04 +0900

    Toggle과 Remove 기능 추가

--- Simple-Todo-App/src/Todo/TodoList/index.tsx
+++ Simple-Todo-App/src/Todo/TodoList/index.tsx
@@ -1,12 +1,35 @@
 import { observer } from "mobx-react-lite";
-import TodoStore from "../../stores/TodoStore";
+import TodoStore, { Todo } from "../../stores/TodoStore";
 import styles from "./TodoList.module.css";
 
 const TodoList = ({ todos }: { todos: TodoStore }) => {
+  const handleToggleTodo = (t: Todo) => () => {
+    todos.toggle(t);
+  };
+
+  const handleRemoveTodo = (t: Todo) => () => {
+    todos.remove(t);
+  };
+
   return (
     <ul className={styles["todo-list"]}>
       {todos.list.map((t) => (
-        <li key={t.id}>{t.title}</li>
+        <li key={t.id}>
+          <label htmlFor={String(t.id)} className={t.isDone ? styles.done : ""}>
+            {t.title}
+          </label>
+
+          <button
+            onClick={handleRemoveTodo(t)}
+            className={[styles.remove, t.isDone && styles.done].join("  ")}
+          >
+            remove
+          </button>
+
+          <button onClick={handleToggleTodo(t)}>
+            <input type="checkbox" id={String(t.id)} readOnly tabIndex={-1} />
+          </button>
+        </li>
       ))}
     </ul>
   );
