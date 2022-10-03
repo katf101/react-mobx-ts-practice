4f66023012fa596f868e7844748ed958a2baae74
hansol <102638663+katf101@users.noreply.github.com>
2022-10-03 14:47:45 +0900

    add, toggle, remove 기능 추가

--- Simple-Todo-App/src/stores/TodoStore.ts
+++ Simple-Todo-App/src/stores/TodoStore.ts
@@ -4,4 +4,26 @@ export interface Todo {
   isDone: boolean;
 }
 
-class TodoStore {}
+class TodoStore {
+  list: Todo[] = [];
+
+  add(title: string) {
+    if (title.length < 3) {
+      return;
+    }
+
+    this.list.push({
+      id: Date.now(),
+      title,
+      isDone: false,
+    });
+  }
+
+  toggle(todo: Todo) {
+    todo.isDone = !todo.isDone;
+  }
+
+  remove(todo: Todo) {
+    this.list = this.list.filter((t) => t.id !== todo.id);
+  }
+}
