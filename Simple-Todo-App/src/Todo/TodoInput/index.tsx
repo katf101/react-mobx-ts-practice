c60d8d3e2d0082c7f33b9147407b408016f2efc2
hansol <102638663+katf101@users.noreply.github.com>
2022-10-03 21:45:28 +0900

    InputChange 기능, Button 추가 기능

--- Simple-Todo-App/src/Todo/TodoInput/index.tsx
+++ Simple-Todo-App/src/Todo/TodoInput/index.tsx
@@ -1,13 +1,17 @@
 import React, { ChangeEvent, useState } from "react";
 import TodoStore from "../../stores/TodoStore";
 
+//
 const TodoInput = ({ todos }: { todos: TodoStore }) => {
   const [newTodo, setNewTodo] = useState("");
 
+  // ts: onchage를 사용할때 e의 타입을 정할때는 ChangeEvent를
+  // 제네릭을 통해 이벤트를 활용하는 HTML 노드 타입을 지정, <input은 HTMLInputElement>
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTodo(e.target.value);
   };
 
+  // Todo 추가 기능
   const handleButtonClick = () => {
     todos.add(newTodo);
     setNewTodo("");
