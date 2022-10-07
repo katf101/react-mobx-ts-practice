3e746416443807a2eb12ee6f20ee05f8c5317c50
hansol <102638663+katf101@users.noreply.github.com>
2022-10-07 18:02:04 +0900

    using decorators

--- 21-MobX-And-Decorators/src/stores/TodoStore.ts
+++ 21-MobX-And-Decorators/src/stores/TodoStore.ts
@@ -10,20 +10,25 @@ export interface Todo {
 class TodoStore {
   list: Todo[] = [];
 
+  // constructor() {
+  //   // 객체를 observable로 만드는 가장 기본적인 방법은 makeObservable를 사용하여 속성마다 주석을 지정하는 것
+  //   // 프로퍼티, 객체, 배열 , map, set은 observable로 설정될 수 있음
+  //   makeObservable(this, {
+  //     // observable state 생성
+  //     list: observable,
+  //     // action 선언
+  //     // action은 state를 수정하는 메서드를 표시
+  //     add: action,
+  //     toggle: action,
+  //     remove: action,
+  //   });
+  // }
+
   constructor() {
-    // 객체를 observable로 만드는 가장 기본적인 방법은 makeObservable를 사용하여 속성마다 주석을 지정하는 것
-    // 프로퍼티, 객체, 배열 , map, set은 observable로 설정될 수 있음
-    makeObservable(this, {
-      // observable state 생성
-      list: observable,
-      // action 선언
-      // action은 state를 수정하는 메서드를 표시
-      add: action,
-      toggle: action,
-      remove: action,
-    });
+    makeObservable(this);
   }
 
+  @action
   add(title: string) {
     if (title.length < 3) {
       return;
@@ -36,10 +41,12 @@ class TodoStore {
     });
   }
 
+  @action
   toggle(todo: Todo) {
     todo.isDone = !todo.isDone;
   }
 
+  @action
   remove(todo: Todo) {
     this.list = this.list.filter((t) => t.id !== todo.id);
   }
