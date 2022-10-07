d5526b67069ed94b8d26d31d52276b9a6f69b3d3
hansol <102638663+katf101@users.noreply.github.com>
2022-10-07 16:54:44 +0900

    useStore 사용을 위한 props 삭제

--- Simple-Todo-App/src/App.tsx
+++ Simple-Todo-App/src/App.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import TodoStore from "./stores/TodoStore";
+// import TodoStore from "./stores/TodoStore";
 import TodoInput from "./Todo/TodoInput";
 import TodoList from "./Todo/TodoList";
 
 // TodoStore를 가져와 todos에 할당
-const todos = new TodoStore();
+// const todos = new TodoStore();
 
 const App = () => {
   return (
     <div className="app">
-      <TodoInput todos={todos} />
-      <TodoList todos={todos} />
+      <TodoInput />
+      <TodoList />
     </div>
   );
 };
