71c1e915f8e1f61376946fc75b9df9aaf1a37f2f
hansol <102638663+katf101@users.noreply.github.com>
2022-10-07 17:41:08 +0900

    update

---
+++ 20-Connect-MobX-React-ContextAPI/src/Todo/TodoInput/index.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useStore } from "../../stores";
+import TodoStore from "../../stores/TodoStore";
+import styles from "./TodoInput.module.css";
+
+// TodoStore 사용
+const TodoInput = () => {
+  const { todos } = useStore();
+
+  const [newTodo, setNewTodo] = useState("");
+
+  // ts: onchage를 사용할때 event(e)의 타입을 정할때는 ChangeEvent를
+  // 제네릭을 통해 이벤트를 활용하는 HTML 노드 타입을 지정, <input은 HTMLInputElement>
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
+
+  // ts: subimt의 event(e)의 타입은 FormEvent로 지정
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    todos.add(newTodo);
+    setNewTodo("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["todo-input-group"]}>
+      <input value={newTodo} onChange={handleInputChange} />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default TodoInput;
---
+++ 20-Connect-MobX-React-ContextAPI/src/stores/TodoStore.ts
@@ -0,0 +1,48 @@
+import exp from "constants";
+import { action, makeObservable, observable } from "mobx";
+
+export interface Todo {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
+
+class TodoStore {
+  list: Todo[] = [];
+
+  constructor() {
+    // 객체를 observable로 만드는 가장 기본적인 방법은 makeObservable를 사용하여 속성마다 주석을 지정하는 것
+    // 프로퍼티, 객체, 배열 , map, set은 observable로 설정될 수 있음
+    makeObservable(this, {
+      // observable state 생성
+      list: observable,
+      // action 선언
+      // action은 state를 수정하는 메서드를 표시
+      add: action,
+      toggle: action,
+      remove: action,
+    });
+  }
+
+  add(title: string) {
+    if (title.length < 3) {
+      return;
+    }
+
+    this.list.push({
+      id: Date.now(),
+      title,
+      isDone: false,
+    });
+  }
+
+  toggle(todo: Todo) {
+    todo.isDone = !todo.isDone;
+  }
+
+  remove(todo: Todo) {
+    this.list = this.list.filter((t) => t.id !== todo.id);
+  }
+}
+
+export default TodoStore;
