102fe1bdac11160c6d12819e70ee9e6cd8000d14
hansol <102638663+katf101@users.noreply.github.com>
2022-10-07 12:18:11 +0900

    useStore 생성

--- Simple-Todo-App/src/stores/index.tsx
+++ Simple-Todo-App/src/stores/index.tsx
@@ -1,7 +1,15 @@
+import { createContext, useContext } from "react";
 import TodoStore from "./TodoStore";
 
 const store = {
   todos: new TodoStore(),
+};
+
+// context 생성
+export const StoreContext = createContext(store);
+
+export const useStore = () => {
+  return useContext<typeof store>(StoreContext);
 };
 
 export default store;
